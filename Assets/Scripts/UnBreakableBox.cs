c1956bc8df022fcd660251348c3ed8d248135b84
Amin-MAG <M.Amin.Ghasvari@gmail.com>
2021-11-18 00:33:28 +0330

    feat: add un breackable boxes

---
+++ Assets/Scripts/UnBreakableBox.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnBreakableBox : Box
+{
+    void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        base.Update();
+    }
+
+    protected override int GetStartingHealth()
+    {
+        return 999999999;
+    }
+}
