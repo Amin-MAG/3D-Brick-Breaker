877d77912498fbaedde6973f9027281756e08087
Amin-MAG <M.Amin.Ghasvari@gmail.com>
2021-11-18 00:33:41 +0330

    feat: add iron boxes

---
+++ Assets/Scripts/IronBoxController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IronBoxController : Box
+{
+    protected override int GetStartingHealth()
+    {
+        return 3;
+    }
+
+    public void Start()
+    {
+        base.Start();
+    }
+
+    public void Update()
+    {
+        base.Update();
+    }
+}
