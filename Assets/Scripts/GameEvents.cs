3df5bc44cd8ff62c9f40dc913ebf4cbdf8f0b628
Amin-MAG <M.Amin.Ghasvari@gmail.com>
2021-11-17 23:26:45 +0330

    feat: add game events

---
+++ Assets/Scripts/GameEvents.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.PlayerLoop;
+using UnityEngine.SceneManagement;
+
+public class GameEvents : MonoBehaviour
+{
+    public UnityEvent onGameOver;
+
+    void Awake()
+    {
+        onGameOver = new UnityEvent();
+    }
+
+    private void Update()
+    {
+        // Reload Game
+        if (Input.GetKey(KeyCode.R) && Input.GetKey(KeyCode.LeftShift))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
